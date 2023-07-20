cb509fdc478c132be51f22365ba65466ac03d062
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-20 17:56:30 -0500

    Create main.py

---
+++ Python/Modulos/main.py
@@ -0,0 +1,11 @@
+from ejercicio_1.Operaciones import sumar, restar, multiplicar, dividir
+
+resultado_suma = sumar(3, 5)
+resultado_resta = restar(10, 4)
+resultado_multipliacion = multiplicar(2, 6)
+resultado_division = dividir(15, 3)
+
+print("Suma:", resultado_suma)
+print("Resta:", resultado_resta)
+print("Multipliacion:", resultado_multipliacion)
+print("Division.", resultado_division)
